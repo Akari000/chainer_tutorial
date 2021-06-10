ab8eab9817e1302ba371cc3ba70e6bff38ec6606
Akari Haga <akarihaga@AkarinoMacBook-Pro.local>
2021-06-11 00:45:28 +0900

    add 3_1

---
+++ Pytorch_tutorial/3_1/main.py
@@ -0,0 +1,115 @@
+import math
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchtext
+from torchtext.data.utils import get_tokenizer
+from positionalEncoding import PositionalEncoding
+from transformerModel import TransformerModel
+from utils import batchify
+import torch
+import torchtext
+from torchtext.data.utils import get_tokenizer
+from utils import batchify, get_batch
+
+
+def train(criterion, optimizer, scheduler):
+    model.train() # 訓練モードに
+    total_loss = 0.
+    start_time = time.time()
+    ntokens = len(TEXT.vocab.stoi)
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
+        data, targets = get_batch(train_data, i, bptt)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output.view(-1, ntokens), targets)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        optimizer.step()
+
+        total_loss += loss.item()
+        log_interval = 200
+        if batch % log_interval == 0 and batch > 0:
+            cur_loss = total_loss / log_interval
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:5d}/{:5d} batches | '
+                  'lr {:02.2f} | ms/batch {:5.2f} | '
+                  'loss {:5.2f} | ppl {:8.2f}'.format(
+                    epoch, batch, len(train_data) // bptt, scheduler.get_last_lr()[0],
+                    elapsed * 1000 / log_interval,
+                    cur_loss, math.exp(cur_loss)))
+            total_loss = 0
+            start_time = time.time()
+
+def evaluate(eval_model, data_source, criterion):
+    eval_model.eval() # 検証モードに
+    total_loss = 0.
+    ntokens = len(TEXT.vocab.stoi)
+    with torch.no_grad():
+        for i in range(0, data_source.size(0) - 1, bptt):
+            data, targets = get_batch(data_source, i, bptt)
+            output = eval_model(data)
+            output_flat = output.view(-1, ntokens)
+            total_loss += len(data) * criterion(output_flat, targets).item()
+    return total_loss / (len(data_source) - 1)
+
+
+TEXT = torchtext.data.Field(tokenize=get_tokenizer("basic_english"),
+                            init_token='<sos>',
+                            eos_token='<eos>',
+                            lower=True)
+train_txt, val_txt, test_txt = torchtext.datasets.WikiText2.splits(TEXT)
+TEXT.build_vocab(train_txt)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+bptt = 35
+
+
+# インスタンスの初期化
+ntokens = len(TEXT.vocab.stoi) # the size of vocabulary
+emsize = 200 # embedding dimension
+nhid = 200 # the dimension of the feedforward network model in nn.TransformerEncoder
+nlayers = 2 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+nhead = 2 # the number of heads in the multiheadattention models
+dropout = 0.2 # the dropout value
+model = TransformerModel(ntokens, emsize, nhead, nhid, nlayers, dropout).to(device)
+
+# モデルの実行
+batch_size = 20
+eval_batch_size = 10
+train_data = batchify(train_txt, batch_size, TEXT, device)
+val_data = batchify(val_txt, eval_batch_size, TEXT, device)
+test_data = batchify(test_txt, eval_batch_size, TEXT, device)
+
+criterion = nn.CrossEntropyLoss()
+lr = 5.0 # 学習率
+optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+best_val_loss = float("inf")
+epochs = 3 # The number of epochs
+best_model = None
+
+
+for epoch in range(1, epochs + 1):
+    epoch_start_time = time.time()
+    train(criterion, optimizer, scheduler)
+    val_loss = evaluate(model, val_data, criterion)
+    print('-' * 89)
+    print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+          'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
+                                     val_loss, math.exp(val_loss)))
+    print('-' * 89)
+
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        best_model = model
+
+    scheduler.step()
+
+# 評価
+test_loss = evaluate(best_model, test_data)
+print('=' * 89)
+print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
+    test_loss, math.exp(test_loss)))
+print('=' * 89)
